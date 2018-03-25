14f0d2799a7115ec59ecdd684990d214f7a9fa7b
KYHSGeekCode <jourhyang123@gmail.com>
2018-03-26 00:52:37 +0900

    Initial commit

---
+++ app/src/main/java/com/kyunggi/worker2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kyunggi.worker2;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
