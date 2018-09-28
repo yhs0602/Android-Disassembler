a2934812fc1820a05712e64c511fd8b3f288bffd
root <root@localhost>
2018-09-29 00:13:35 +0900

    Initial commit

---
+++ app/src/main/java/com/kyhsgeekcode/disassembler/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kyhsgeekcode.disassembler;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
