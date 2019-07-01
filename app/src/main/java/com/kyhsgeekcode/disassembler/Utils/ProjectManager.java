161f50d1c9031c41cafb12099b278f4b56be8b7b
KYHSGeekCode <jourhyang123@gmail.com>
2019-07-01 13:42:26 +0900

    Add udd constants

--- app/src/main/java/com/kyhsgeekcode/disassembler/Utils/ProjectManager.java
+++ app/src/main/java/com/kyhsgeekcode/disassembler/Utils/ProjectManager.java
@@ -1,11 +1,17 @@
 package com.kyhsgeekcode.disassembler.Utils;
 
+import android.util.Log;
+
 import com.kyhsgeekcode.disassembler.MainActivity;
+import com.kyhsgeekcode.disassembler.Utils.Olly.UddTag;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProjectManager {
+    private static final String TAG = "Disasm PM_new";
     public static void Write() {
 
     }
@@ -14,20 +20,23 @@ public class ProjectManager {
 
     }
 
+    static Map<UddTag, byte[]> uddData = new HashMap<>();
+
     static void ReadUDD(DataInputStream is) throws IOException {
         int type;
         int length;
         byte[] data;
-
+        uddData.clear();
         while (is.available() > 0) {
             type = is.readInt() & 0xFFffFFff;
             length = is.readInt() & 0xFFffFFff;
             data = new byte[length];
             is.read(data, 0, length);
-            switch (type) {
-                //case :
-                //    break;
+            UddTag tag = UddTag.fromInt(type);
+            if (tag == UddTag.TAG_UNKNOWN) {
+                Log.w(TAG, "Unknown type:" + type);
             }
+            uddData.put(tag, data);
         }
     }
 }
