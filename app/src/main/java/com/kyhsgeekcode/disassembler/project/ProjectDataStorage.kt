15e4e8a23d4ac091a8a42628953a65bd56ea1d00
KYHSGeekCode <jourhyang123@gmail.com>
2020-02-20 13:51:16 +0900

    :sparkles: Implement getFileContent

--- app/src/main/java/com/kyhsgeekcode/disassembler/project/ProjectDataStorage.kt
+++ app/src/main/java/com/kyhsgeekcode/disassembler/project/ProjectDataStorage.kt
@@ -1,9 +1,21 @@
 package com.kyhsgeekcode.disassembler.project
 
-import com.kyhsgeekcode.disassembler.TabTags
+import kotlinx.serialization.UnstableDefault
 
 object ProjectDataStorage {
-    //Pair of relPath and tabtag
-    val data: Map<Pair<String, String>, Any> = HashMap()
+    //Pair of relPath and dataType
+    val data: MutableMap<Pair<String, DataType>, Any> = HashMap()
 
+    @UnstableDefault
+    fun getFileContent(relPath: String): ByteArray {
+        val key = Pair(relPath, DataType.FileContent)
+        if (data.containsKey(key)) {
+            data[key] = ProjectManager.getOriginal(relPath).readBytes()
+        }
+        return data[key] as ByteArray
+    }
+}
+
+enum class DataType {
+    FileContent;
 }
