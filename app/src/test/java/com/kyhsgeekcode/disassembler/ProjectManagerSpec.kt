09b7c930311da0e58720547d3de920501f5c087b
KYHSGeekCode <jourhyang123@gmail.com>
2021-01-29 23:26:42 +0900

    :bug: Fix test

--- app/src/test/java/com/kyhsgeekcode/disassembler/ProjectManagerSpec.kt
+++ app/src/test/java/com/kyhsgeekcode/disassembler/ProjectManagerSpec.kt
@@ -1,10 +1,8 @@
 package com.kyhsgeekcode.disassembler
 
-import kotlinx.serialization.UnstableDefault
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
-@UnstableDefault
 object ProjectManagerSpec : Spek({
     //    ProjectManager.currentProject = ProjectModel("TestProject", )
     describe("GetRelPath") {
