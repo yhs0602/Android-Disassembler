d5cacd8b129292f3b946f686d40815babb0e71f5
KYHSGeekCode <jourhyang123@gmail.com>
2021-01-30 21:41:32 +0900

    :white_check_mark: Fix test

--- app/src/test/java/com/kyhsgeekcode/disassembler/ProjectManagerSpec.kt
+++ app/src/test/java/com/kyhsgeekcode/disassembler/ProjectManagerSpec.kt
@@ -2,12 +2,15 @@ package com.kyhsgeekcode.disassembler
 
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertEquals
 
 object ProjectManagerSpec : Spek({
     //    ProjectManager.currentProject = ProjectModel("TestProject", )
     describe("GetRelPath") {
-        context("") {
-
+        context("context") {
+            it("SHould pass") {
+                assertEquals(expected = 4, actual = 4)
+            }
         }
     }
 })
