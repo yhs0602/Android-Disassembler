a893e11618dc6dd218968b832b764e2d3ba6f110
KYHSGeekCode <jourhyang123@gmail.com>
2021-02-21 12:53:01 +0900

    :recycle: Refactor disasm

--- app/src/main/java/com/kyhsgeekcode/disassembler/AssemblyProvider.java
+++ app/src/main/java/com/kyhsgeekcode/disassembler/AssemblyProvider.java
@@ -7,43 +7,12 @@ import java.util.List;
 
 public abstract class AssemblyProvider {
     DisasmListViewAdapter adapter;
-    private long total;
 
-    public AssemblyProvider(DisasmListViewAdapter adapter, /*Unused */long total) {
-        this.total = total;
+    public AssemblyProvider(DisasmListViewAdapter adapter /*Unused */) {
         this.adapter = adapter;
     }
 
     public abstract long getAll(int handle, byte[] bytes, long offset, long size, long virtaddr);
 
     public abstract long getSome(int handle, byte[] bytes, long offset, long size, long virtaddr, int count);
-
-    //Used by JNI
-    public void AddItem(final DisassemblyListItem lvi) {
-        new Handler(Looper.getMainLooper()).post(() -> {
-            long addr = lvi.disasmResult.address;
-            AbstractFile theFile = adapter.getFile();
-            List<Symbol> syms = theFile.getExportSymbols();
-            for (Symbol sym : syms) {
-                if (sym.st_value == addr) {
-                    lvi.AddComment(sym.demangled);
-                    break;
-                }
-            }
-
-            if (lvi.disasmResult.isCall()) {
-                if (theFile instanceof ElfFile) {
-                    ElfFile theElfFile = (ElfFile) theFile;
-                    long target = lvi.disasmResult.getJumpOffset();
-                    int pltIndex = theElfFile.getPltIndexFromJumpAddress(target);
-                    if (pltIndex > 0) {
-                        ImportSymbol theSym = theFile.getImportSymbols().get(pltIndex);
-                        lvi.AddComment(theSym.getDemangled() + "@plt");
-                    }
-                }
-            }
-            adapter.addItem(lvi);
-            adapter.notifyDataSetChanged();
-        });
-    }
 }
