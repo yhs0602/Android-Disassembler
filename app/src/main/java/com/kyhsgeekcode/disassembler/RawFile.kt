88b26dac72a225fa424a9fc747f1058447653b70
KYHSGeekCode <jourhyang123@gmail.com>
2022-01-01 13:46:43 +0900

    :fire: Remove files

---
+++ app/src/main/java/com/kyhsgeekcode/disassembler/RawFile.kt
@@ -0,0 +1,10 @@
+package com.kyhsgeekcode.disassembler
+
+import java.io.File
+
+class RawFile(file: File, filecontent: ByteArray?) : AbstractFile() {
+    init {
+        fileContents = filecontent!!
+        path = file.path
+    }
+}
